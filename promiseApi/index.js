5cc323b542ecc70e75ef732f08dbae0a99f5c66b
Jerry <jerry.wang@xuenn.com>
2021-03-26 12:58:48 +0800

    fetch is nonblock code, other line will keep do

--- promiseApi/index.js
+++ promiseApi/index.js
@@ -1,6 +1,11 @@
+var temp = {};
+
 fetch('https://jsonplaceholder.typicode.com/todos/1')
   .then(response => response.json())
-  .then(json => console.log(json))
+  .then((json) => {
+    temp = json;
+    console.log(temp);
+  });
 
 
-
+console.log(temp);
